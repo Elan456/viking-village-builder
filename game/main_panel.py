a8419a30a73c23efe2d764ec9e05199128ae128c
Elan456 <telan4892@gmail.com>
2024-10-18 23:58:25 -0400

    Added resources to main panel

--- game/main_panel.py
+++ game/main_panel.py
@@ -5,6 +5,8 @@ The main panel lives on the bottom of the screen and gives the player a button t
 import pygame
 from config.defines import DISPLAY_WIDTH, DISPLAY_HEIGHT, FONT, FONT_PATH
 from utils.button import Button
+from .resources import get_icon
+from .resources import resource_to_icon
 
 class MainPanel:
     def __init__(self, village) -> None:
@@ -14,16 +16,33 @@ class MainPanel:
                                           FONT, self.next_turn)
         
         self.turn_font = pygame.font.Font(FONT_PATH, 24)
+        self.resource_font = pygame.font.Font(FONT_PATH, 16)
+
+        self.resource_box_width = 100
+        self.resource_box_height = 100
+        self.resource_box = pygame.Surface((self.resource_box_width, self.resource_box_height), pygame.SRCALPHA)
+        self.resource_box.fill((150, 150, 150, 180))
     
         
     def update(self):
         self.next_turn_button.update()
 
     def draw(self, surface):
+        # alpha
+        surface.convert_alpha()
         self.next_turn_button.draw(surface)
 
         current_turn_text = self.turn_font.render(f"Turn: {self.village.turn}", True, (0, 0, 0))
         surface.blit(current_turn_text, (220, DISPLAY_HEIGHT - 100))
+
+        
+        surface.blit(self.resource_box, (DISPLAY_WIDTH//2-self.resource_box_width//2, 0))
+
+        for i, resource in enumerate(resource_to_icon.keys()):
+            icon = get_icon(resource)
+            surface.blit(icon, (DISPLAY_WIDTH//2-self.resource_box_width//2, 0 + i*20))
+            resource_text = self.resource_font.render(f"{self.village.resources[resource]}", True, (0, 0, 0))
+            surface.blit(resource_text, (DISPLAY_WIDTH//2-self.resource_box_width//2 + 50, 0 + i*20))
 
     def next_turn(self):
         self.village.on_new_turn()
--- game/resources.py
+++ game/resources.py
@@ -1,13 +1,17 @@
+import pygame 
+
 resource_to_icon = {
     "wood": "assets/resources/wood.png",
     "ore": "assets/resources/ore.png",
     "food": "assets/resources/food.png",
-    "people": "assets/resources/people.png",
-    "weapons": "assets/resources/weapons.png",
-    "boats": "assets/resources/boats.png",
 }
+
+# Load all the resources and replace the dict with the properly scaled images
+for resource_name, resource_path in resource_to_icon.items():
+    image = pygame.image.load(resource_path)
+    image = pygame.transform.scale(image, (24, 24))
+    resource_to_icon[resource_name] = image
 
 
 def get_icon(resource_name: str):
     return resource_to_icon[resource_name]
-
