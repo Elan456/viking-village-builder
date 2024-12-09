0faf471dd87963e1e9a7e1dab5398729cb99a715
Elan456 <telan4892@gmail.com>
2024-12-09 15:52:20 -0500

    Add fps counter, caption, and icon

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ from config.defines import DISPLAY_WIDTH, DISPLAY_HEIGHT, FULL_SCREEN
 from events.announcements import announcement_handler
 from game.start_menu import StartMenu
 from game.lore_scroll import LoreScroll
+from config.defines import FONT_PATH
 
 pygame.init()
 pygame.font.init()
 pygame.mixer.init()
+pygame.display.set_caption("Invasion of London: Forkbeard's Call")
+pygame.display.set_icon(pygame.image.load("assets/resources/axe.png"))
 
 class Game:
     def __init__(self) -> None:
@@ -20,6 +23,8 @@ class Game:
         self.village = Village(self.event_handler)
         self.lore_scroll = LoreScroll(self.event_handler)
         self.clock = pygame.time.Clock()
+
+        self.fps_font = pygame.font.Font(FONT_PATH, 12)
 
     def start(self):
         while True:
@@ -34,9 +39,12 @@ class Game:
             announcement_handler.update()
             announcement_handler.display(self.screen)
 
+            fps_text = self.fps_font.render(f"{frame_rate:.1f} FPS", True, (0, 0, 0))
+            self.screen.blit(fps_text, (DISPLAY_WIDTH - fps_text.get_width() - 5, 5))
+
+
             pygame.display.flip()
             self.clock.tick(30)
-            pygame.display.set_caption(f"FPS: {frame_rate}")
 
 
 
